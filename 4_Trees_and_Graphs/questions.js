a025ce315d77aa536c4089b8a4db9e33303dc14c
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-11-11 13:44:41 -0800

    Finish 4/3

--- 4_Trees_and_Graphs/questions.js
+++ 4_Trees_and_Graphs/questions.js
@@ -2,22 +2,49 @@
 // Given a sorted (increasing) order array with unique integer elements, 
 // write an algorithm to create a binary search tree with minimal innerHeight
 
+// class Node {
+//   constructor(val = null, parent = null, left = null, right = null) {
+//     this.value = val;
+//     this.parent = parent;
+//     this.left = left;
+//     this.right = right;
+//   }
+
+//   left(node) {
+//     this.left = node;
+//   }
+
+//   right(node) {
+//     this.right = node;
+//   }
+
+// }
+
 class Node {
-  constructor(val = null, parent = null, left = null, right = null) {
+  constructor(val = null, next = null) {
     this.value = val;
-    this.parent = parent;
-    this.left = left;
-    this.right = right;
+    this.next= next;
   }
 
-  left(node) {
-    this.left = node;
+  next(node) {
+    this.next = node;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+    this.tail = null;
   }
 
-  right(node) {
-    this.right = node;
+  append(node) {
+    if (this.head) {
+      this.head.next(node);
+      this.tail = node;
+    } else {
+      this.head = node;
+    }
   }
-
 }
 
 // [1,2,3,4,5,6,7]
@@ -29,3 +56,16 @@ function minTree(array, node = null) {
   curr.right = minTree(array.slice(mid + 1));
 }
 
+// 4.3
+// Given a binary tree, design an algorithm which creates a linked list of all the nodes 
+// at each depth 
+function listOfDepth(node, arr = [], list = new LinkedList) {
+  if (node === null) {
+    arr.push(list); 
+    list = new LinkedList;
+    return;
+  }
+  list.append(node);
+  listOfDepth(node.left);
+  listOfDepth(node.right);
+}
