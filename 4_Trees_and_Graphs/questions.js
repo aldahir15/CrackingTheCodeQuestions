946cc2342c7f84bc4a7124054a14133174e29e83
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-11-12 16:18:18 -0800

    Finish chpt 4

--- 4_Trees_and_Graphs/questions.js
+++ 4_Trees_and_Graphs/questions.js
@@ -122,3 +122,128 @@ function buildOrder(projects, dependencies) {
     dep[1].parent = dep[0];
   });
 }
+
+//4.8 
+// Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree. 
+// Avoid storing additional nodes in a data structure.
+function firstCommonAncestor(node1, node2, arr = []) {
+  if (node1 === null && node2 === null) return null;
+  if (node1 === node2) {
+    return node1;
+  }
+  if (arr.includes(node1) || arr.includes(node2)) {
+    if (arr.includes(node1)) {
+      return node1;
+    } else {
+      return node2;
+    }
+  }
+  arr.push(node1, node2);
+  return firstCommonAncestor(node1.parent, node2.parent, arr);
+}
+
+// 4.9
+// A binary search tree was created by traversing through an array from left to right and inserting each element.
+// Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
+
+function bstSequences(bst) {
+  let sequences = [];
+  let recurse = function(nodes, traveled) {
+    let noChild = true;
+    nodes.forEach((node) => {
+      if (node.left !== null && traveled[node.left.value] === undefined) {
+        noChild = false;
+        traveled[node.left.value] = true;
+        recurse(nodes.concat([node.left]), traveled);
+        delete traveled[node.left.value];
+      }
+      if (node.right !== null && traveled[node.right.value] === undefined) {
+        noChild = false;
+        traveled[node.right.value] = true;
+        recurse(node.concat([node.right]), traveled);
+        delete traveled[node.right.value];
+      }
+    });
+    if (noChild) {
+      sequences.push(nodes.map(node => node.value));
+    }
+  };
+  let startTravelled = {};
+  startTravelled[bst.value] = true;
+  recurse([bst], startTravelled);
+  return sequences;
+}
+
+// 4.10 
+
+function checkSubtree(tree1, tree2) {
+  const queue = [tree1];
+  let curr = null;
+  while (queue.length >= 1) {
+    queue.forEach(node => {
+      queue.push(node.left);
+      queue.push(node.right);
+      if (node.left === tree2) {
+        curr = node.left;
+      } else {
+        curr = node.right;
+      }
+      queue.shift;
+    });
+  }
+  return checkEqual(tree1, tree2);
+}
+
+function checkEqual(tree1, tree2, bool = true) {
+  if (tree1 !== tree2) {
+   bool = false;
+   return bool; 
+  }
+  checkEqual(tree1.left, tree2.left, bool) || checkEqual(tree1.right, tree2.right, bool);
+  return bool;
+}
+
+// 4.11
+
+function randomNode(tree, arr = []) {
+  if (tree === null) return;
+  arr.push(tree);
+  randomNode(tree.left, arr) || randomNode(tree.right, arr);
+  return arr[parseInt(Math.random() * (arr.length))];
+}
+
+// 4.12
+function sumArr(arr) {
+  return arr.reduce((total, num) => total + num, 0);
+}
+
+function countPathsWithRoot(tree, value, path) {
+  let pathCount = 0;
+  if (path === undefined) {
+    path = [tree.value];
+  } else {
+    path = [...path, tree.value];
+  }
+  if (sumArr(path) === value) {
+    pathCount ++;
+  }
+  if (tree.left !== null) {
+    pathCount += countPathsWithRoot(tree.left, value, path);
+  }
+  if (tree.right !== null) {
+    pathCount += countPathsWithRoot(tree.right, value, path);
+  }  
+  return pathCount;
+}
+
+function pathsWithSum(tree, value) {
+  let pathCount = 0;
+  pathCount += countPathsWithRoot(tree, value);
+  if (tree.left !== null) {
+    pathCount += countPathsWithRoot(tree.left, value);
+  }
+  if (tree.right !== null) {
+    pathCount += countPathsWithRoot(tree.right, value);
+  }
+  return pathCount;
+}
