d2e298c10b656457e28fd0bb3b50792051c5e089
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-11-15 15:58:54 -0800

    6/4

--- 6_Math_and_Logic/questions.js
+++ 6_Math_and_Logic/questions.js
@@ -49,3 +49,13 @@
 // a domino needs a black square and a white square to occupy the chessboard 
 
 // 6.4
+
+
+// they do not collide only if:
+// clockwise, clockwise, clockwise or
+// anticlockwise, anticlockwise, anticlockwise
+
+// since the probability of not colliding is 2 / (2 * 2 * 2)
+// then the probability of colliding is 1 - 2 / (2 ^ 3)
+
+// the generalized solution for non-collision is 1 - 2 / (2 ^ n)
